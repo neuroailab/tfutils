2f59f1adc2a0ed603e6af98dacc5f8229586275b
Dan yamins <dyamins@gmail.com>
2016-12-25 18:17:15 -0500

    minor

--- tfutils/model.py
+++ tfutils/model.py
@@ -214,7 +214,7 @@ def mnist(inputs, train=True, **kwargs):
 
 def alexnet(inputs, train=True, **kwargs):
     m = ConvNet(**kwargs)
-    reuse = None if train else True
+    reuse = None #if train else True
 
     with tf.contrib.framework.arg_scope([m.conv], init='xavier',
                                         stddev=.01, bias=.1, activation='relu'):
