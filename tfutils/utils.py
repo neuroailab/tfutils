8b662b32664baac09f3c7eed6e0c6939d338ef2a
Eli <ewang921@gmail.com>
2017-03-16 18:45:29 -0700

    temporarily removed pkg_resources error

--- tfutils/utils.py
+++ tfutils/utils.py
@@ -39,7 +39,7 @@ def version_info(module):
         pkgname = module.__name__.split('.')[0]
         try:
             info = pkg_resources.get_distribution(pkgname)
-        except (pkg_resources.DistributionNotFound, pkg_resources.RequirementParseError):
+        except (pkg_resources.DistributionNotFound):#, pkg_resources.RequirementParseError):
             version = None
             log.warning('version information not found for %s -- what package is this from?' % module.__name__)
         else:
