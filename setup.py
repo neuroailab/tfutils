70b7e56d8c071f4227747bb163502195be58b7f7
Aran Nayebi <anayebi@stanford.edu>
2018-04-10 02:22:23 +0000

    Removed tensorflow-gpu requirement

--- setup.py
+++ setup.py
@@ -75,7 +75,8 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['h5py', 'numpy', 'tensorflow-gpu', 'zmq', 'pymongo', 'tqdm', 'gitpython'],
+    # install_requires=['h5py', 'numpy', 'tensorflow-gpu', 'zmq', 'pymongo', 'tqdm', 'gitpython'],
+    install_requires=['h5py', 'numpy', 'zmq', 'pymongo', 'tqdm', 'gitpython'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
