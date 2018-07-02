355be6d8a99ce49be32f3cabc5f566ca260d567b
anayebi <anayebi@stanford.edu>
2018-07-02 10:36:42 -0700

    Added clip min and clip max

--- tfutils/optimizer.py
+++ tfutils/optimizer.py
@@ -16,9 +16,11 @@ log.setLevel('DEBUG')
 
 class ClipOptimizer(object):
 
-    def __init__(self, optimizer_class, clip=True, trainable_names=None, *optimizer_args, **optimizer_kwargs):
+    def __init__(self, optimizer_class, clip=True, clip_min=-1.0, clip_max=1.0, trainable_names=None, *optimizer_args, **optimizer_kwargs):
         self._optimizer = optimizer_class(*optimizer_args, **optimizer_kwargs)
         self.clip = clip
+        self.clip_min = clip_min
+        self.clip_max = clip_max
         self.var_list = None
         if not isinstance(trainable_names, list) and trainable_names is not None:
             trainable_names = [trainable_names]
@@ -47,7 +49,7 @@ class ClipOptimizer(object):
         if self.clip:
             # gradient clipping. Some gradients returned are 'None' because
             # no relation between the variable and loss; so we skip those.
-            gvs = [(tf.clip_by_value(grad, -1., 1.), var)
+            gvs = [(tf.clip_by_value(grad, self.clip_min, self.clip_max), var)
                    for grad, var in gvs if grad is not None]
         return gvs
 
