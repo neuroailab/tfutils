3302687abb654718759efd7915fdd4c63de14523
Chengxu Zhuang <chengxuz@node07-ccncluster.stanford.edu>
2019-01-22 18:39:22 -0800

    Minor fix for loading from cache database

--- tfutils/db_interface.py
+++ tfutils/db_interface.py
@@ -607,7 +607,14 @@ class DBInterface(object):
         try:
             count_recent = collfs_recent.find(query).count()
         except Exception as inst:
-            raise er.OperationFailure(inst.args[0] + "\n Is your dbname too long? Mongo requires that dbnames be no longer than 64 characters.")
+            log.info(inst.args[0] \
+                    + "\n Is your dbname too long? "\
+                    + "Mongo requires that dbnames be no longer "\
+                    + "than 64 characters.")
+            log.info("Will not load from cache database.")
+            count_recent = 0
+            #raise er.OperationFailure(inst.args[0] + "\n Is your dbname too long? Mongo requires that dbnames be no longer than 64 characters.")
+
         if count_recent > 0:  # get latest that matches query
             ckpt_record_recent = coll_recent.find(query, sort=[('uploadDate', -1)])[0]
             # use the record with latest timestamp
