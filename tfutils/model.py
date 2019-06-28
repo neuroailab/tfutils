4e59734d41da34c431cab9b940ca355ee068d475
Aran Nayebi <anayebi@stanford.edu>
2019-06-27 18:45:36 -0700

    Fixed differences and added cg bn

--- tfutils/model.py
+++ tfutils/model.py
@@ -5,17 +5,72 @@ from collections import OrderedDict
 from contextlib import contextmanager
 
 import tensorflow as tf
+import numpy as np
 
 def initializer(kind='xavier', *args, **kwargs):
     if kind == 'xavier':
         init = tf.contrib.layers.xavier_initializer(*args, **kwargs)
+    elif kind == 'normal':
+        init = normal_initializer
     else:
         init = getattr(tf, kind + '_initializer')(*args, **kwargs)
+            
     return init
 
+def normal_initializer(shape, dtype=None, partition_info=None):
+    '''
+    Used for EfficientNets
+    '''
+    H, W, _, C_out = shape
+    fan_out = int(H * W * C_out)
+    return tf.random_normal(
+        shape, mean=0.0, stddev=np.sqrt(2.0 / fan_out), dtype=dtype)
+
+def groupnorm(inputs, G=32, data_format='channels_last', weight_decay=0.0, epsilon=1e-5, trainable=True, gamma_init=1, beta_init=0):
+    '''
+    Like LayerNorm, z-scores features along the channel dimension only. 
+    However, it only normalizes within G groups of C/G channels each.
+    Optionally applies learnable scale/shift parameters.
+    '''
+    assert len(inputs.shape.as_list()) == 4, "Applies only to conv2D layers"
+    if data_format == 'channels_first':
+        inputs = tf.transpose(inputs, [0,2,3,1])
+    elif data_format == 'channels_last':
+        pass
+    else:
+        raise ValueError("data_format must be 'channels_first' or 'channels_last'")
+
+    B,H,W,C = inputs.shape.as_list()
+    assert C % G == 0, "num groups G must divide C"
+    CpG = C // G
+
+    inputs = tf.reshape(inputs, [B,H,W,CpG,G])
+    mean, var = tf.nn.moments(inputs, axes=[1,2,3], keep_dims=True)
+    inputs = tf.div(inputs - mean, tf.sqrt(var + epsilon))
+    inputs = tf.reshape(inputs, [B,H,W,C])
+    
+    if trainable:
+        gamma = tf.get_variable("groupnorm_scale", shape=[1,1,1,C], dtype=tf.float32,
+                                initializer=initializer("constant", float(gamma_init)))
+                                # regularizer=tf.contrib.layers.l2_regularizer(weight_decay))
+        beta = tf.get_variable("groupnorm_shift", shape=[1,1,1,C], dtype=tf.float32,
+                               initializer=initializer("constant", float(beta_init)))
+                               # regularizer=tf.contrib.layers.l2_regularizer(weight_decay))
+    else:
+        gamma = tf.constant(gamma_init, dtype=tf.float32)
+        beta = tf.constant(beta_init, dtype=tf.float32)
+
+    inputs = gamma*inputs + beta
+    if data_format == 'channels_first':
+        inputs = tf.transpose(inputs, [0,3,1,2])
+
+    print("applied group norm to", inputs.name.split('/')[:-1])
+
+    return inputs
+
 def batchnorm_corr(inputs, is_training, data_format='channels_last', 
-    decay = 0.9, epsilon = 1e-5, init_zero=None, constant_init=None, activation=None,
-    time_suffix=None):
+    decay = 0.9, epsilon = 1e-5, init_zero=None, constant_init=None, 
+    activation=None, time_suffix=None, use_crossgpu_bn=False, num_dev=None):
 
     if time_suffix is not None:
         bn_op_name = "post_conv_BN_" + time_suffix
@@ -35,20 +90,31 @@ def batchnorm_corr(inputs, is_training, data_format='channels_last',
     else:
         gamma_init = tf.constant_initializer(constant_init)
 
-    axis = 1 if data_format == 'channels_first' else 3
-    output = tf.layers.batch_normalization(inputs=inputs,
-                                           axis=axis,
-                                           momentum=decay,
-                                           epsilon=epsilon,
-                                           center=True,
-                                           scale=True,
-                                           training=is_training,
-                                           trainable=True,
-                                           fused=True,
-                                           gamma_initializer=gamma_init,
-                                           name=bn_op_name,
-                                           reuse=reuse_flag)
-
+    if use_crossgpu_bn:
+        output = crossgpu_batch_norm(inputs=inputs,
+                                     decay=decay,
+                                     epsilon=epsilon,
+                                     is_training=is_training,
+                                     data_format=data_format,
+                                     trainable=True,
+                                     gamma_initializer=gamma_init,
+                                     scope=bn_op_name,
+                                     reuse=reuse_flag,
+                                     num_dev=num_dev)
+    else:
+        axis = 1 if data_format == 'channels_first' else 3
+        output = tf.layers.batch_normalization(inputs=inputs,
+                                               axis=axis,
+                                               momentum=decay,
+                                               epsilon=epsilon,
+                                               center=True,
+                                               scale=True,
+                                               training=is_training,
+                                               trainable=True,
+                                               fused=True,
+                                               gamma_initializer=gamma_init,
+                                               name=bn_op_name,
+                                               reuse=reuse_flag)
     return output
 
 def conv(inp,
@@ -64,14 +130,18 @@ def conv(inp,
          weight_decay=None,
          activation='relu',
          batch_norm=False,
+         group_norm=False,
+         num_groups=32,
          is_training=False,
          batch_norm_decay=0.9,
          batch_norm_epsilon=1e-5,
+         batch_norm_gamma_init=None,
          init_zero=None,
          dropout=None,
          dropout_seed=0,
          time_sep=False,
          time_suffix=None,
+         crossgpu_bn_kwargs={'use_crossgpu_bn': False},
          name='conv'
          ):
     
@@ -80,13 +150,20 @@ def conv(inp,
     if time_sep:
         assert time_suffix is not None
 
+    if batch_norm or group_norm:
+        use_bias = False
+
     if weight_decay is None:
         weight_decay = 0.
     if isinstance(ksize, int):
         ksize = [ksize, ksize]
+    if isinstance(strides, int):
+        strides = [1, strides, strides, 1]
     if kernel_init_kwargs is None:
         kernel_init_kwargs = {}
     in_depth = inp.get_shape().as_list()[-1]
+    if out_depth is None:
+        out_depth = in_depth
 
     # weights
     init = initializer(kernel_init, **kernel_init_kwargs)
@@ -105,7 +182,7 @@ def conv(inp,
                             name='bias')
     # ops
     if dropout is not None:
-        inp = tf.nn.dropout(inp, dropout, seed=dropout_seed, name='dropout')
+        inp = tf.nn.dropout(inp, keep_prob=dropout, seed=dropout_seed, name='dropout')
 
     conv = tf.nn.conv2d(inp, kernel,
                         strides=strides,
@@ -121,11 +198,20 @@ def conv(inp,
                                 is_training=is_training, 
                                 data_format=data_format, 
                                 decay = batch_norm_decay, 
-                                epsilon = batch_norm_epsilon, 
+                                epsilon = batch_norm_epsilon,
+                                constant_init=batch_norm_gamma_init,
                                 init_zero=init_zero, 
                                 activation=activation,
-                                time_suffix=time_suffix)
-
+                                time_suffix=time_suffix,
+                                **crossgpu_bn_kwargs)
+    elif group_norm:
+        output = groupnorm(inputs=output,
+                           G=num_groups,
+                           data_format=data_format,
+                           weight_decay=weight_decay,
+                           gamma_init=(0.0 if init_zero else 1.0),
+                           epsilon=batch_norm_epsilon)
+        
     if activation is not None:
         output = getattr(tf.nn, activation)(output, name=activation)
 
@@ -143,12 +229,21 @@ def conv_bnf(inp,
          activation='relu6',
          batch_norm=True,
          is_training=True,
-         batch_norm_decay = 0.9,
-         batch_norm_epsilon = 1e-5,
+         batch_norm_decay=0.9,
+         batch_norm_epsilon=1e-5,
+         init_zero=None,
+         data_format='channels_last',
+         time_sep=False,
+         time_suffix=None,
+         crossgpu_bn_kwargs={'use_crossgpu_bn': False},
          name='conv_bnf'
          ):
 
     # assert out_shape is not None
+
+    if time_sep:
+        assert time_suffix is not None
+
     if weight_decay is None:
         weight_decay = 0.
     if isinstance(ksize, int):
@@ -174,7 +269,16 @@ def conv_bnf(inp,
                         padding=padding)
 
     if batch_norm:
-        output = batchnorm_corr(output, is_training=is_training, decay = batch_norm_decay, epsilon = batch_norm_epsilon)
+        # if activation is none, should use zeros; else ones
+        output = batchnorm_corr(inputs=output, 
+                                is_training=is_training, 
+                                data_format=data_format, 
+                                decay = batch_norm_decay, 
+                                epsilon = batch_norm_epsilon, 
+                                init_zero=init_zero, 
+                                activation=activation,
+                                time_suffix=time_suffix,
+                                **crossgpu_bn_kwargs)
     else:
         init = initializer(kind='constant', value=bias)
         biases = tf.get_variable(initializer=init,
@@ -225,23 +329,36 @@ def depthsep_conv(inp,
     return p_out
 
 def depth_conv(inp,
-             multiplier=1,
-             ksize=3,
-             strides=1,
-             padding='SAME',
-             kernel_init='xavier',
-             kernel_init_kwargs=None,
-             bias=1,
-             activation='relu6',
-             weight_decay=None,
-             batch_norm = True,
-             is_training=True,
-             batch_norm_decay = 0.9,
-             batch_norm_epsilon = 1e-5,
-             name='depth_conv'
+               multiplier=1,
+               out_depth=None,
+               ksize=3,
+               strides=1,
+               padding='SAME',
+               kernel_init='xavier',
+               kernel_init_kwargs=None,
+               activation='relu6',
+               weight_decay=None,
+               batch_norm = False,
+               group_norm=False,
+               num_groups=32,
+               use_bias=False,
+               is_training=True,
+               batch_norm_decay=0.9,
+               batch_norm_epsilon=1e-5,
+               batch_norm_gamma_init=None,
+               init_zero=None,
+               data_format='channels_last',
+               time_sep=False,
+               time_suffix=None,
+               crossgpu_bn_kwargs={'use_crossgpu_bn': False},
+               name='depth_conv'
              ):
 
     # assert out_shape is not None
+
+    if time_sep:
+        assert time_suffix is not None
+
     if weight_decay is None:
         weight_decay = 0.
     if isinstance(ksize, int):
@@ -264,20 +381,37 @@ def depth_conv(inp,
                             regularizer=tf.contrib.layers.l2_regularizer(weight_decay),
                             name='weights')
 
-    conv = tf.nn.depthwise_conv2d(inp, kernel,
+    output = tf.nn.depthwise_conv2d(inp, kernel,
                             strides=strides,
                             padding=padding)
         
     if batch_norm:
-        output = batchnorm_corr(output, is_training=is_training, decay = batch_norm_decay, epsilon = batch_norm_epsilon)
-    else:
-        init = initializer(kind='constant', value=bias)
+        output = batchnorm_corr(inputs=output, 
+                                is_training=is_training, 
+                                data_format=data_format, 
+                                decay = batch_norm_decay, 
+                                epsilon = batch_norm_epsilon,
+                                constant_init=batch_norm_gamma_init,
+                                init_zero=init_zero, 
+                                activation=activation,
+                                time_suffix=time_suffix,
+                                **crossgpu_bn_kwargs)
+    elif group_norm:
+        output = groupnorm(inputs=output,
+                           G=num_groups,
+                           data_format=data_format,
+                           weight_decay=weight_decay,
+                           gamma_init=(0.0 if init_zero else 1.0),
+                           epsilon=batch_norm_epsilon)
+        
+    elif use_bias:
+        init = initializer(kind='constant', value=1.0)
         biases = tf.get_variable(initializer=init,
                                 shape=[out_depth],
                                 dtype=tf.float32,
                                 regularizer=tf.contrib.layers.l2_regularizer(weight_decay),
                                 name='bias')
-        output = tf.nn.bias_add(conv, biases, name=name)   
+        output = tf.nn.bias_add(output, biases, name=name)   
 
     if activation is not None:
         output = getattr(tf.nn, activation)(output, name=activation)
@@ -301,7 +435,11 @@ def fc(inp,
        dropout_seed=0,
        time_sep=False,
        time_suffix=None,
+       crossgpu_bn_kwargs={'use_crossgpu_bn': False},
        name='fc'):
+
+    if batch_norm:
+        use_bias = False
         
     if weight_decay is None:
         weight_decay = 0.
@@ -329,7 +467,7 @@ def fc(inp,
 
     # ops
     if dropout is not None:
-        resh = tf.nn.dropout(resh, dropout, seed=dropout_seed, name='dropout')
+        resh = tf.nn.dropout(resh, keep_prob=dropout, seed=dropout_seed, name='dropout')
     fcm = tf.matmul(resh, kernel)
 
     if use_bias:
@@ -355,7 +493,19 @@ def fc(inp,
             bn_op_name = "post_conv_BN"
             reuse_flag = None
 
-        output = tf.layers.batch_normalization(inputs=output,
+        use_crossgpu_bn = crossgpu_bn_kwargs.pop('use_crossgpu_bn', False)
+        if use_crossgpu_bn:
+            output = crossgpu_batch_norm(inputs=inputs,
+                                                 decay=batch_norm_decay,
+                                                 epsilon=batch_norm_epsilon,
+                                                 training=is_training,
+                                                 trainable=True,
+                                                 gamma_initializer=gamma_init,
+                                                 scope=bn_op_name,
+                                                 reuse=reuse_flag,
+                                                 **crossgpu_bn_kwargs)
+        else:
+            output = tf.layers.batch_normalization(inputs=output,
                                                axis=-1,
                                                momentum=batch_norm_decay,
                                                epsilon=batch_norm_epsilon,
